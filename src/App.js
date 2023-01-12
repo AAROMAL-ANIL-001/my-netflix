199e053fb2142362d7ce84228ddb90c5f7a12676
AAROMAL-ANIL-001 <110799825+AAROMAL-ANIL-001@users.noreply.github.com>
2023-01-12 09:15:58 +0530

    donn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import NavBar from './Components/Navbar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+import {originals,action,comedy,horror,documentary} from './url'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={comedy} title='Comedy' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
+      <RowPost url={documentary} title='Documentary' isSmall/>
+
     </div>
   );
 }
---
+++ src/constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl ='https://api.themoviedb.org/3';
+export const API_KEY="4b22ba488d204b0198028e6c373cba22";
+export const imageUrl ='https://image.tmdb.org/t/p/original';
